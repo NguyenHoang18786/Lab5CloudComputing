922e437b16f0495a8ac97a15474245358c49121c
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-24 23:08:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
   <body>
 
-    <center><h1>Test Heroku web applicationg with Github repository</h1></center>
+    <center><h1>Test Heroku web application with Github repository</h1></center>
 
     <?php
       echo "Hello Cloud Computing Class!";
