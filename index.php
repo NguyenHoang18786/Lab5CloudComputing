77c5886c463088bb82817134aaf286564b09df5e
NguyenHoang18786 <66770116+NguyenHoang18786@users.noreply.github.com>
2021-02-24 22:50:45 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <body>
+
+    <center><h1>Test Heroku web applicationg with Github repository</h1></center>
+
+    <?php
+      echo "Hello Cloud Computing Class!";
+      echo "<p>Nguyễn Hoàng</p>";
+    ?>
+
+  </body>
+</html>
